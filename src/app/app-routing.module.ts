41bd465863eb78d358900a8af389a9e30c6ef0fa
SufieCo <sufiyan.solanki@gmail.com>
2023-12-13 16:16:37 +0530

    header working modules created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'cars', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) }, { path: 'bike', loadChildren: () => import('./bike/bike.module').then(m => m.BikeModule) }, { path: 'health', loadChildren: () => import('./health/health.module').then(m => m.HealthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
